3b337ac585f9f77e7ce8679683e3e93652c218cc
fdt-naoki-sakamoto <naoki.sakamoto@fastdoctor.jp>
2023-10-28 21:33:44 +0900

    https://www.udemy.com/course/tailwindcss-for-beginner/learn/lecture/32520930#announcements

--- projects/udemy-tailwind/tailwind.config.ts
+++ projects/udemy-tailwind/tailwind.config.ts
@@ -13,6 +13,13 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        primary: {
+          100: "#ebf8ff",
+          300: "#90cdf4",
+          500: "#3182ce",
+        }
+      }
     },
   },
   plugins: [],
