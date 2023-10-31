f9c2a3b9a220f1b5051c943536ef3df594681596
fdt-naoki-sakamoto <naoki.sakamoto@fastdoctor.jp>
2023-10-31 23:39:57 +0900

    https://www.udemy.com/course/tailwindcss-for-beginner/learn/lecture/32435560#announcements

--- projects/udemy-tailwind/src/app/page.tsx
+++ projects/udemy-tailwind/src/app/page.tsx
@@ -4,21 +4,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <div className="border border-blue-400 py-16 px-16 my-10">
-        <h1 className="text-7xl font-bold sm:text-red-400 md:text-blue-400 lg:text-purple-400 xl:text-green-400">
-          TailwindCSS入門
-        </h1>
-      </div>
-      <div className="border border-blue-400 py-16 px-16 my-10">
-        <h1 className="lg:text-7xl text-3xl font-bold">TailwindCSS入門</h1>
-      </div>
-      <button className="easy-button" onClick={() => console.warn("hello")}>
-        ボタン(素css)
-      </button>
-      <button className="bg-blue-400 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded-xl ml-10">
-        ボタン(tw)
-      </button>
-      <button className="buton bg-primary-500">ボタン(tw)</button>
+      <h1 className="text-3l font-bold">Hello TailWind</h1>
     </>
   );
 }
